944863d4210d98a44dc6915f474a7cced2951520
Omega <aldjomega@gmail.com>
2023-09-28 16:57:10 +1000

    first commit

---
+++ dist/constants.d.ts
@@ -0,0 +1,36 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    TESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x0f2C36CcE00036DFD3e3569061ADFE935a7237cE";
+export declare const FACTORY_ADDRESS_PCS = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+export declare const INIT_CODE_HASH = "0xf6dc550707c6a3f739f89eaee3b594aab3e1904c398231e0902bd1f9e0eb7cb0";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
